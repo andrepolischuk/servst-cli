fea2d3c403cc8d0e0474e5c20392ade32c1f43e5
andrepolischuk <andre.polischuk@gmail.com>
2015-10-16 23:56:29 +0300

    Add test

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+var test = require('ava');
+var exec = require('child_process').exec;
+
+test('should return version', function (t) {
+  t.plan(2);
+
+  exec('node cli.js --version', {cwd: __dirname}, function (err, stdout) {
+    t.ifError(err);
+    t.true(stdout.trim().length > 0);
+  });
+});
